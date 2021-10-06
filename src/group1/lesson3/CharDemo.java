380a7ebdd1b9c70c8c63344c4f9c255deb30594b
ReStart Computers <ani-gasparyan-2021@mai.ru>
2021-10-06 20:57:40 +0400

    change BoolTest, CharDemo,CharDemo2,Convesion, Dynlnit, LifeTime,Ppromote,Scope

--- src/group1/lesson3/CharDemo.java
+++ src/group1/lesson3/CharDemo.java
@@ -6,7 +6,7 @@ public class CharDemo {
         char ch1, ch2;
         ch1 = 88;
         ch2 = 'Y';
-        System.out.println("ch1 и ch2");
+        System.out.print("ch1 и ch2");
         System.out.println(ch1 + " " + ch2);
     }
 
---
+++ src/group1/lesson4/Array.java
@@ -0,0 +1,9 @@
+package group1.lesson4;
+
+public class Array {
+
+    public static void main(String[] args) {
+
+    }
+
+}
