c3445406388e3db94908d04b8d1d8645a32fbfc7
ReStart Computers <ani-gasparyan-2021@mai.ru>
2021-10-09 13:30:47 +0400

    change BoolTest, CharDemo,CharDemo2,Convesion, Dynlnit, LifeTime,Ppromote,Scope

--- src/Homeworke/FigurePainter2.java
+++ src/Homeworke/FigurePainter2.java
@@ -2,7 +2,21 @@ package Homeworke;
 
 public class FigurePainter2 {
 
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 1; j++) {
+                System.out.println("       " + "*");
+            }
+            for (int k = 0; k < 4; k++) {
+                System.out.print("* " + "* " +"* "+ "*");
+            }
+            System.out.println();
 
+        }
+    }
 }
+
+
+
 
 
--- src/Homeworke/FigurePainter3.java
+++ src/Homeworke/FigurePainter3.java
@@ -1,6 +1,6 @@
 package Homeworke;
 
-public class Homework3 {
+public class FigurePainter3 {
 
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
