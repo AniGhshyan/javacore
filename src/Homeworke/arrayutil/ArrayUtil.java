1434221a4287947580daa49d4f085998e3818c67
ReStart Computers <ani-gasparyan-2021@mai.ru>
2021-10-15 22:10:28 +0400

    Change classes DoWhile, FindPrime, IfElseIf, Menu, MissingBreak, NoBody, SampleSwitch, Switch, While

--- src/Homeworke/arrayutil/ArrayUtil.java
+++ src/Homeworke/arrayutil/ArrayUtil.java
@@ -1,4 +1,82 @@
 package Homeworke.arrayutil;
 
 public class ArrayUtil {
-}
+
+    public static void main(String[] args) {
+
+        int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
+        System.out.println();
+
+        for (int i = 0; i < array.length; i++) {
+                if (array[i] >=66)
+
+                    System.out.println(array[i]);
+            }
+
+
+            System.out.println();
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] <= 2)
+                    System.out.println(array[i]);
+
+            }
+            System.out.println();
+
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 == 0)
+                    System.out.println(array[i]);
+
+            }
+            System.out.println();
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 > 0)
+                    System.out.println(array[i]);
+
+            }
+            System.out.println();
+
+            int a = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 == 0) {
+                    a++;
+                }
+            }
+            System.out.println(a);
+
+            System.out.println();
+
+            int b = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 > 0)
+                    b++;
+
+            }
+            System.out.println(b);
+
+            System.out.println();
+
+            double c;
+            int k = 0;
+            for (int i = 0; i < array.length; i++) {
+                k += array[i];
+            }
+            c = k / array.length;
+            System.out.println(c);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
