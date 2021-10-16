08a845319eb2b65896bf34ec74f4ac117463698c
ReStart Computers <ani-gasparyan-2021@mai.ru>
2021-10-16 16:02:43 +0400

    Change classes DoWhile, FindPrime, IfElseIf, Menu, MissingBreak, NoBody, SampleSwitch, Switch, While

--- src/Homeworke/arrayutil/ArrayUtil.java
+++ src/Homeworke/arrayutil/ArrayUtil.java
@@ -6,69 +6,81 @@ public class ArrayUtil {
 
         int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
         for (int i = 0; i < array.length; i++) {
+
             System.out.println(array[i]);
 
         }
         System.out.println();
 
+        int max = array[0];
         for (int i = 0; i < array.length; i++) {
-                if (array[i] >=66)
-
-                    System.out.println(array[i]);
-            }
-
-
-            System.out.println();
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] <= 2)
-                    System.out.println(array[i]);
+            int a = array[i];
+            if (a > max) {
+                max = a;
 
             }
-            System.out.println();
 
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] % 2 == 0)
-                    System.out.println(array[i]);
-
-            }
-            System.out.println();
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] % 2 > 0)
-                    System.out.println(array[i]);
-
-            }
-            System.out.println();
-
-            int a = 0;
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] % 2 == 0) {
-                    a++;
-                }
-            }
-            System.out.println(a);
-
-            System.out.println();
-
-            int b = 0;
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] % 2 > 0)
-                    b++;
-
-            }
-            System.out.println(b);
-
-            System.out.println();
-
-            double c;
-            int k = 0;
-            for (int i = 0; i < array.length; i++) {
-                k += array[i];
-            }
-            c = k / array.length;
-            System.out.println(c);
         }
+        System.out.println(max);
 
+
+        System.out.println();
+
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            int a = array[i];
+            if (a < min)
+                min = a;
+
+        }
+        System.out.println(min);
+
+
+        System.out.println();
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0)
+                System.out.println(array[i]);
+
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 > 0)
+                System.out.println(array[i]);
+
+        }
+        System.out.println();
+
+        int a = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                a++;
+            }
+        }
+        System.out.println(a);
+
+        System.out.println();
+
+        int b = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 > 0)
+                b++;
+
+        }
+        System.out.println(b);
+
+        System.out.println();
+
+        double c;
+        int k = 0;
+        for (int i = 0; i < array.length; i++) {
+            k += array[i];
+        }
+        c = k / array.length;
+        System.out.println(c);
     }
+
+}
 
 
 
