834a932b21ea30b9fcd8574c16247fdafc7b28e7
ReStart Computers <ani-gasparyan-2021@mai.ru>
2021-10-18 17:47:12 +0400

    Change classes DoWhile, FindPrime, IfElseIf, Menu, MissingBreak, NoBody, SampleSwitch, Switch, While

--- src/Homeworke/arrayutil/ArrayUtil.java
+++ src/Homeworke/arrayutil/ArrayUtil.java
@@ -7,77 +7,72 @@ public class ArrayUtil {
         int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
         for (int i = 0; i < array.length; i++) {
 
-            System.out.println(array[i]);
+            System.out.print(array[i] + " ");
 
         }
         System.out.println();
 
         int max = array[0];
-        for (int i = 0; i < array.length; i++) {
-            int a = array[i];
-            if (a > max) {
-                max = a;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
 
             }
 
         }
-        System.out.println(max);
+        System.out.print("max = " + max);
 
 
         System.out.println();
 
         int min = array[0];
-        for (int i = 0; i < array.length; i++) {
-            int a = array[i];
-            if (a < min)
-                min = a;
-
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min)
+                min = array[i];
         }
-        System.out.println(min);
-
+        System.out.print("min = " + min);
 
         System.out.println();
 
+        System.out.println("Մասիվի զույգ տարրերն են");
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0)
-                System.out.println(array[i]);
+                System.out.print(array[i] + " ");
 
         }
         System.out.println();
+        System.out.println("Մասիվի կենտ տարրերն են");
         for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 > 0)
-                System.out.println(array[i]);
+            if (array[i] % 2 != 0)
+                System.out.print(array[i] +" ");
 
         }
         System.out.println();
-
+        System.out.println("Մասիվի զույգ տարրերի քանակ.");
         int a = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
                 a++;
             }
         }
-        System.out.println(a);
+        System.out.print(a);
 
         System.out.println();
 
+        System.out.println("Մասիվի կենտ տարրերի քանակ.");
         int b = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 > 0)
                 b++;
-
         }
-        System.out.println(b);
+        System.out.print(b);
 
         System.out.println();
-
-        double c;
-        int k = 0;
+        double k = 0;
         for (int i = 0; i < array.length; i++) {
             k += array[i];
         }
-        c = k / array.length;
-        System.out.println(c);
+        System.out.println("Միջին թվաբանականը = " + k/ array.length);
     }
 
 }
